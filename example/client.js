024f2101ec73e9cfb043d0246943d4e5697529c0
Nathan Carter <ncarter@bentley.edu>
2020-04-05 18:15:42 -0400

    Extend example to a simple chat client

--- example/client.js
+++ example/client.js
@@ -1,6 +1,15 @@
 
 connect()
 
+ModelClass = MapModel
+
+window.changed = ( key, oldValue, newValue ) => {
+    if ( key == 'chats' ) {
+        document.getElementById( 'chats' ).innerHTML =
+            newValue.map( line => `<p>${line}</p>` ).join( '\n' )
+    }
+}
+
 window.heard = msg => {
     console.log( `I (client ${clientId}) heard from server: ${msg}` )
     say( 'I heard you!' )
@@ -9,3 +18,7 @@ window.heard = msg => {
         () => say( `Now I have ID ${askFor}.` ),
         () => say( `Aw, I wanted to be ${askFor}.` ) )
 }
+
+document.getElementById( 'send' ).addEventListener( 'click', event => {
+    say( { chat : document.getElementById( 'nextChat' ).value } )
+} )
--- example/server.js
+++ example/server.js
@@ -1,23 +1,35 @@
 
 const { GatherRound } = require( '../server' )
+const { MapModel } = require( '../map-model' )
 const path = require( 'path' )
 
 const server = new GatherRound()
-server.addScript( path.join( __dirname, 'client.js' ) )
-server.setMainPageFile( path.join( __dirname, 'index.html' ) )
-server.onStart = port => console.log( `Started on port ${port}` )
 
+const model = new MapModel()
+server.setModel( model )
+model.set( 'chats', [ ] )
+
+server.onStart = port => console.log( `Started on port ${port}` )
 server.onConnect = client => {
     let counter = 0
     while ( server.getClient( counter ) ) counter++
     client.setId( counter )
     console.log( `Connected client ${client.id}` )
     client.tell( 'Can you hear me?' )
-    client.heard = msg => console.log( `Client ${client.id} said: ${msg}` )
+    client.heard = msg => {
+        if ( msg.hasOwnProperty( 'chat' ) ) {
+            model.set( 'chats', [ ...model.get( 'chats' ),
+                                  `${client.id} says: ${msg.chat}` ] )
+        }
+    }
     console.log( 'Current client IDs:', server.clients.map( c => c.id ) )
 }
 server.onDisconnect = client => {
     console.log( `Disconnected client ${client.id}` )
     console.log( 'Current client IDs:', server.clients.map( c => c.id ) )
 }
+
+server.addScript( path.join( __dirname, '..', 'map-model.js' ) )
+server.addScript( path.join( __dirname, 'client.js' ) )
+server.setMainPageFile( path.join( __dirname, 'index.html' ) )
 server.start()
