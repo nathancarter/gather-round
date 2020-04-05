fe6f2a9c54903c63ea705b578e2716a2a634d36b
Nathan Carter <ncarter@bentley.edu>
2020-04-05 15:00:58 -0400

    Incorporate HTML file into example

--- example/server.js
+++ example/server.js
@@ -4,6 +4,7 @@ const path = require( 'path' )
 
 const server = new GatherRound()
 server.setClientScriptFile( path.join( __dirname, 'client.js' ) )
+server.setMainPageFile( path.join( __dirname, 'index.html' ) )
 server.onStart = port => console.log( `Started on port ${port}` )
 server.onConnect = () => console.log( 'Connection' )
 server.onDisconnect = () => console.log( 'Disconnection' )
