68e5854120eace4388c3655cc58c2250c9b38e9b
Nathan Carter <ncarter@bentley.edu>
2020-04-05 13:56:06 -0400

    Use socket.io, eliminate index.html

--- server.js
+++ server.js
@@ -2,9 +2,30 @@
 let app = require( 'express' )()
 let http = require( 'http' ).createServer( app )
 let path = require( 'path' )
+let io = require( 'socket.io' )( http )
 
 app.get( '/', ( request, result ) => {
-    result.sendFile( path.join( __dirname, 'index.html' ) )
+    result.send(
+`
+<html>
+    <head>
+        <script src='/socket.io/socket.io.js'></script>
+        <script>
+            let socket = io()
+        </script>
+    </head>
+    <body>
+        <p>Empty page.</p>
+    </body>
+</html>
+` )
+} )
+
+io.on( 'connection', socket => {
+    console.log( 'Connection started' )
+    socket.on( 'disconnect', () => {
+        console.log( 'Connection ended' )
+    } )
 } )
 
 http.listen( 9999, () => {
