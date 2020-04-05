e46f4929528780365571a878802e6e05be9162cc
Nathan Carter <ncarter@bentley.edu>
2020-04-05 14:58:20 -0400

    Create example use

---
+++ example/server.js
@@ -0,0 +1,10 @@
+
+const { GatherRound } = require( '../server' )
+const path = require( 'path' )
+
+const server = new GatherRound()
+server.setClientScriptFile( path.join( __dirname, 'client.js' ) )
+server.onStart = port => console.log( `Started on port ${port}` )
+server.onConnect = () => console.log( 'Connection' )
+server.onDisconnect = () => console.log( 'Disconnection' )
+server.start()
