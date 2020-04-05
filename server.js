2f69e65913229701a893f2f85398976f7f1a298a
Nathan Carter <ncarter@bentley.edu>
2020-04-05 14:12:58 -0400

    Pack socket.io into client

--- server.js
+++ server.js
@@ -3,22 +3,27 @@ let app = require( 'express' )()
 let http = require( 'http' ).createServer( app )
 let path = require( 'path' )
 let io = require( 'socket.io' )( http )
+let fs = require( 'fs' )
 
 app.get( '/', ( request, result ) => {
     result.send(
 `
 <html>
     <head>
-        <script src='/socket.io/socket.io.js'></script>
-        <script>
-            let socket = io()
-        </script>
+        <script src='client.js'></script>
     </head>
     <body>
         <p>Empty page.</p>
     </body>
 </html>
 ` )
+} )
+
+const socketClient = fs.readFileSync( path.join(
+    __dirname, 'node_modules', 'socket.io-client', 'dist', 'socket.io.js' ) )
+const myClient = fs.readFileSync( path.join( __dirname, 'client.js' ) )
+app.get( '/client.js', ( request, result ) => {
+    result.send( `${socketClient}\n\n${myClient}` )
 } )
 
 io.on( 'connection', socket => {
@@ -31,4 +36,3 @@ io.on( 'connection', socket => {
 http.listen( 9999, () => {
     console.log( 'Server started' )
 } )
-
