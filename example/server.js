b68e1e59e4009d53c2a3a26c773924120a2bdd74
Nathan Carter <ncarter@bentley.edu>
2020-04-06 11:18:22 -0400

    Enable multiple rooms/models

--- example/server.js
+++ example/server.js
@@ -1,34 +1,44 @@
 
-const { GatherLobby } = require( '../server' )
+const { GatherLobby, GatherRoom } = require( '../server' )
 const { MapModel } = require( '../map-model' )
 const path = require( 'path' )
 
-const server = new GatherLobby()
-
-const model = new MapModel()
-server.setModel( model )
-model.set( 'chats', [ ] )
-
-server.onStart = port => console.log( `Started on port ${port}` )
-server.onConnect = socket => {
-    console.log( `Connected client ${socket.id}` )
-    socket.say( 'Can you hear me?' )
-    socket.heard = msg => {
-        if ( msg.hasOwnProperty( 'chat' ) ) {
-            model.set( 'chats', [ ...model.get( 'chats' ),
-                                  `${socket.id} says: ${msg.chat}` ] )
-        } else {
-            console.log( `Heard from ${socket.id}: ${msg}` )
+const makeChatRoom = server => {
+    const model = new MapModel()
+    server.setModel( model )
+    model.set( 'chats', [ ] )
+    const old = server.onConnect
+    server.onConnect = socket => {
+        if ( old ) old( socket )
+        socket.heard = msg => {
+            if ( msg.hasOwnProperty( 'chat' ) ) {
+                model.set( 'chats', [ ...model.get( 'chats' ),
+                                      `${socket.id} says: ${msg.chat}` ] )
+            } else {
+                console.log( `Heard from ${socket.id}: ${msg}` )
+            }
         }
     }
-    console.log( 'Current client IDs:', server.sockets.map( c => c.id ) )
-}
-server.onDisconnect = socket => {
-    console.log( `Disconnected client ${socket.id}` )
-    console.log( 'Current client IDs:', server.sockets.map( c => c.id ) )
 }
 
-server.addScript( path.join( __dirname, '..', 'map-model.js' ) )
-server.addScript( path.join( __dirname, 'client.js' ) )
-server.setMainPageFile( path.join( __dirname, 'index.html' ) )
-server.start()
+const lobby = new GatherLobby()
+lobby.onStart = port => console.log( `Started on port ${port}` )
+lobby.onConnect = socket => {
+    console.log( `Connected client ${socket.id}` )
+    socket.say( 'Can you hear me?' )
+    console.log( 'Current client IDs:', lobby.sockets.map( c => c.id ) )
+}
+lobby.onDisconnect = socket => {
+    console.log( `Disconnected client ${socket.id}` )
+    console.log( 'Current client IDs:', lobby.sockets.map( c => c.id ) )
+}
+lobby.addScript( path.join( __dirname, '..', 'map-model.js' ) )
+lobby.addScript( path.join( __dirname, 'client.js' ) )
+makeChatRoom( lobby )
+lobby.addRoomFile( '/chatroom', path.join( __dirname, 'index.html' ), () => {
+    const room = new GatherRoom()
+    makeChatRoom( room )
+    return room
+} )
+lobby.setMainPageFile( path.join( __dirname, 'index.html' ) )
+lobby.start()
--- server.js
+++ server.js
@@ -4,71 +4,33 @@ const http = require( 'http' )
 const path = require( 'path' )
 const io = require( 'socket.io' )
 const fs = require( 'fs' )
+const url = require( 'url' )
 
 const socketClient = fs.readFileSync( path.join(
     __dirname, 'node_modules', 'socket.io-client', 'dist', 'socket.io.js' ) )
 const gatherClient = fs.readFileSync( path.join( __dirname, 'client.js' ) )
 const injection = '[[[INJECTION-FLAG]]]'
+const injected = ( html ) =>
+    /<head>/i.test( html ) ? html.replace( /<head>/i, `<head>${injection}` ) :
+    /<html>/i.test( html ) ? html.replace( /<html>/i, `<html>${injection}` ) :
+    /<body>/i.test( html ) ? html.replace( /<body>/i, `${injection}<body>` ) :
+                             injection + html
 
-class GatherLobby {
+class GatherRoom {
     constructor () {
-        this.app = app()
-        this.http = http.createServer( this.app )
-        this.io = io( this.http )
-        this.port = 80
-        this.setMainPageHTML( '' )
         this.sockets = [ ]
-        this.scripts = [ ]
     }
-    setMainPageFile ( absolutePath ) {
-        this.setMainPageHTML( String( fs.readFileSync( absolutePath ) ) )
-    }
-    setMainPageHTML ( html ) {
-        if ( /<head>/i.test( html ) ) {
-            html = html.replace( /<head>/i, `<head>${injection}` )
-        } else if ( /<html>/i.test( html ) ) {
-            html = html.replace( /<html>/i, `<html>${injection}` )
-        } else if ( /<body>/i.test( html ) ) {
-            html = html.replace( /<body>/i, `${injection}<body>` )
-        } else {
-            html = injection + html
-        }
-        this.mainPage = html
-    }
-    addScript ( filename ) { this.scripts.push( filename ) }
-    start () {
-        const allScripts = [ 'socket-client.js', 'gather-client.js' ]
-        this.scripts.map( ( filename, index ) => {
-            allScripts.push( `user-script-${index}.js` )
-            this.app.get( `/user-script-${index}.js`, ( request, result ) => {
-                result.sendFile( filename )
-            } )
+    addSocket ( socket ) {
+        socket.gatherRoom = this
+        socket.on( 'client message', msg => {
+            if ( socket.heard ) socket.heard( msg )
         } )
-        const mainPage = this.mainPage.replace( injection,
-            allScripts.map( s => `<script src='${s}'></script>` ).join( '\n' ) )
-        this.app.get( '/', ( request, result ) => { result.send( mainPage ) } )
-        this.app.get( '/socket-client.js', ( request, result ) => {
-            result.send( socketClient )
-        } )
-        this.app.get( '/gather-client.js', ( request, result ) => {
-            result.send( gatherClient )
-        } )
-        this.io.on( 'connection', socket => {
-            socket.gather = this
-            socket.say = function ( msg ) { this.emit( 'server message', msg ) }
-            socket.on( 'client message', msg => {
-                if ( socket.heard ) socket.heard( msg )
-            } )
-            this.sockets.push( socket )
-            if ( this.model ) this.pushModel( socket )
-            if ( this.onConnect ) this.onConnect( socket )
-            socket.on( 'disconnect', () => {
-                this.sockets = this.sockets.filter( c => c != socket )
-                if ( this.onDisconnect ) this.onDisconnect( socket )
-            } )
-        } )
-        this.http.listen( this.port, () => {
-            if ( this.onStart ) this.onStart( this.port )
+        this.sockets.push( socket )
+        if ( this.model ) this.pushModel( socket )
+        if ( this.onConnect ) this.onConnect( socket )
+        socket.on( 'disconnect', () => {
+            this.sockets = this.sockets.filter( c => c != socket )
+            if ( this.onDisconnect ) this.onDisconnect( socket )
         } )
     }
     canSee ( socket, key ) { return true } // subclasses/instances may override
@@ -95,4 +57,75 @@ class GatherLobby {
     }
 }
 
+class GatherLobby extends GatherRoom {
+    constructor () {
+        super()
+        this.app = app()
+        this.http = http.createServer( this.app )
+        this.io = io( this.http )
+        this.port = 80
+        this.setMainPageHTML( '' )
+        this.scripts = [ ]
+        this.otherPages = { }
+        this.rooms = { }
+    }
+    setMainPageFile ( absolutePath ) {
+        this.setMainPageHTML( String( fs.readFileSync( absolutePath ) ) )
+    }
+    setMainPageHTML ( html ) { this.mainPage = html }
+    addScript ( filename ) { this.scripts.push( filename ) }
+    addRoomHTML ( path, html, setup ) { this.otherPages[path] = { setup, html } }
+    addRoomFile ( path, absolutePath, setup ) {
+        this.addRoomHTML( path, String( fs.readFileSync( absolutePath ) ), setup )
+    }
+    start () {
+        const allScripts = [ '/socket-client.js', '/gather-client.js' ]
+        this.scripts.map( ( filename, index ) => {
+            allScripts.push( `/user-script-${index}.js` )
+            this.app.get( `/user-script-${index}.js`, ( request, result ) => {
+                result.sendFile( filename )
+            } )
+        } )
+        const withScripts = ( html ) => injected( html ).replace( injection,
+            allScripts.map( s => `<script src='${s}'></script>` ).join( '\n' ) )
+        this.mainPage = withScripts( this.mainPage )
+        this.app.get( '/', ( request, result ) => result.send( this.mainPage ) )
+        for ( let path in this.otherPages ) {
+            if ( this.otherPages.hasOwnProperty( path ) ) {
+                this.otherPages[path].html =
+                    withScripts( this.otherPages[path].html )
+                console.log( 'Adding other page path:', path + '/*' )
+                this.app.get( path + '/*', ( request, result ) =>
+                    result.send( this.otherPages[path].html ) )
+            }
+        }
+        this.app.get( '/socket-client.js', ( request, result ) =>
+            result.send( socketClient ) )
+        this.app.get( '/gather-client.js', ( request, result ) =>
+            result.send( gatherClient ) )
+        this.io.on( 'connection', socket => {
+            const path = url.parse( socket.handshake.headers.referer ).pathname
+            socket.gatherLobby = this
+            socket.say = function ( msg ) { this.emit( 'server message', msg ) }
+            let target = path == '/' ? this : null
+            for ( let roomPath in this.otherPages ) {
+                if ( this.otherPages.hasOwnProperty( roomPath )
+                  && path.startsWith( roomPath + '/' ) ) {
+                    const roomCode = path.substring( roomPath.length + 1 )
+                    if ( !this.rooms.hasOwnProperty( roomCode ) )
+                        this.rooms[roomCode] = this.otherPages[roomPath].setup()
+                    target = this.rooms[roomCode]
+                    break
+                }
+            }
+            if ( !target ) return console.error( 'Could not connect:', path )
+            target.addSocket( socket )
+        } )
+        this.http.listen( this.port, () => {
+            if ( this.onStart ) this.onStart( this.port )
+        } )
+    }
+}
+
+module.exports.GatherRoom = GatherRoom
 module.exports.GatherLobby = GatherLobby
